cefae5433620d3ee453ec5d2ab2469e2dd41b276
CloseAds <c7336188@gmail.com>
2023-10-31 11:10:31 +0100

    BuclesActu

--- src/PiedraPapelTijera.java
+++ src/PiedraPapelTijera.java
@@ -1,3 +1,15 @@
+
+/*
+
+Diseña un programa que elija entre "piedra", "papel" y "tijeras" al azar. Sin mostrarle la
+elección, pide al usuario que seleccione una de las tres opciones (repetir hasta que lo
+haga, X-Para finalizar el juego). Finalmente, indica qué ha elegido el ordenador y quién ha ganado la partida.
+Recuerda que:
+Piedra machaca las tijeras
+Papel envuelve a la piedra
+Tijeras corta al papel
+ */
+
 import java.util.Scanner;
 
 public class PiedraPapelTijera {
@@ -6,30 +18,74 @@ public class PiedraPapelTijera {
 
         Scanner s = new Scanner(System.in);
 
-        int mínimo = 1;
-        int máximo = 3;
+        char opcion;
+        int elegidoJugador = 0;
+        do{
 
+            System.out.println("Elija una opcion:");
+            System.out.println("A. Piedra");
+            System.out.println("B. Papel");
+            System.out.println("C. Tijera");
+            System.out.println("------------------");
+            System.out.println("X. Para terminar");
 
-        do {
-            System.out.println("método 1: "+(int)(Math.random()*(máximo - mínimo +1)+mínimo));
-            System.out.println("Escoga una opción para jugar");
-            int opción2;
+            opcion = s.next().charAt(0);
 
-            System.out.println("Elige Piedra pulse (A)");
-            System.out.println("Elige Papel pulse (B)");
-            System.out.println("Eliga Tijera pulse (C)");
+            switch (opcion){
+                case 'A':
+                case 'a':
+                    elegidoJugador = 1;
+                    break;
+                case 'B':
+                case 'b':
+                    elegidoJugador = 2;
 
-            int opción1 = s.nextLine().charAt(0);
-
-            if (opción1 == 'A' &&  opción2 == 1){
-                System.out.println("Empate");
-            } else if (opción1 == 'A' &&  opción2== 2) {
-                System.out.println("Has perdido");
-            } else if (opción1 == 'A'&& opción2 == 3){
-                System.out.println("Has ganado");
+                    break;
+                case 'C':
+                case 'c':
+                    elegidoJugador = 3;
+                    break;
             }
+
+            int elegidoOrdenador = (int) (Math.random() * 3 + 1);
+
+            comprobarJuego(elegidoOrdenador, elegidoJugador);
+
+        }while(opcion != 'X');
+    }
+
+
+    public static void comprobarJuego(int elegidoOrdenador, int elegidoJugador){
+
+        boolean ganaOrdenador, ganaJugador;
+        switch (elegidoOrdenador){
+            case 1:
+                System.out.println("El ordenador ha jugado piedra");
+                ganaOrdenador = elegidoJugador == 3;
+                ganaJugador = elegidoJugador == 2;
+                break;
+            case 2:
+                System.out.println("El ordenador ha jugado papel");
+                ganaOrdenador = elegidoJugador == 1;
+                ganaJugador = elegidoJugador == 3;
+
+                break;
+            case 3:
+                System.out.println("El ordenador ha jugado tijera");
+                ganaOrdenador = elegidoJugador == 2;
+                ganaJugador = elegidoJugador == 1;
+                break;
+            default:
+                ganaOrdenador = false;
+                ganaJugador = false;
         }
 
-
+        if (ganaOrdenador) {
+            System.out.println("Gana el ordenador");
+        } else if (ganaJugador) {
+            System.out.println("Gana el jugador");
+        } else {
+            System.out.println("Empate");
+        }
     }
 }
